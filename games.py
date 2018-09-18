35341eccceb20c56a1f37eae1bc93dab3ca05975
Poccket <ccrystal.cclods@gmail.com>
2018-09-18 16:26:51 -0500

    Added pokedex lookup tool

---
+++ games.py
@@ -0,0 +1,135 @@
+import discord
+from discord.ext import commands
+
+from pokedex import pokedex
+pokedex = pokedex.Pokedex()
+
+class Games():
+	'''
+	Gaming related commands
+	'''
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command()
+	async def roll(self, ctx, *, content:str):
+		'''
+		Rolls die, then returns the seperate results and a total
+
+		Usage  : roll <amount> <size>	  
+		amount : Amount of dice to be rolled
+		size   : Size of the dice
+		'''
+		params = content.split()
+		try:
+			int(params[0])
+			int(params[1])
+		except ValueError:
+			await ctx.send("One of your paramaters is not a number\nDo ?help roll to get the usage of this command")
+			return
+		results = []
+		output = "Results: \n`"
+		total = 0
+		for x in range(0, int(params[0])):
+			results.append(random.randint(1,int(params[1])))
+			output += str(results[x]) + ", "
+			total += results[x]
+		output = output[:-2] + "`\nTotal: " + str(total)
+		await ctx.send(output)
+
+	@commands.command()
+	async def pokedex(self, ctx, *, content:str):
+		'''
+		Pokédex lookup tool, returns information about a pokémon
+
+		Usage  : pokedex <number/name>
+		number : Pokémon's Ndex number
+		name   : Pokémon's name
+		'''
+		params = content.split()
+		try:
+			currPoke = pokedex.get_pokemon_by_number(int(params[0]))
+		except ValueError:
+			currPoke = pokedex.get_pokemon_by_name(params[0])
+
+		try:
+			currPoke[0]["name"]
+		except KeyError:
+			await ctx.send("Pokemon does not exist!\nDid you spell it right?")
+			return
+
+		msgContent		= ""
+
+		embedColor		= 0xff0000
+		embedIcon		= "https://upload.wikimedia.org/wikipedia/en/3/39/Pokeball.PNG"
+		embedLink		= "https://bulbapedia.bulbagarden.net/wiki/" + currPoke[0]["name"]
+
+		embedFooter		= "Pokédex Lookup"
+		embedFooterIcon	= "https://cdn.bulbagarden.net/upload/9/9f/Key_Pok%C3%A9dex_m_Sprite.png"
+
+		pName			= currPoke[0]["name"]
+		pSpecies		= currPoke[0]["species"] + " Pokémon"
+		pTypes			= currPoke[0]["types"]
+		pAbilities		= currPoke[0]["abilities"]
+		pEvolutions		= currPoke[0]["family"]["evolutionLine"]
+		pThumbnail		= currPoke[0]["sprite"]
+
+		fTypes			= ""
+		pAbilitiesN		= ""
+		pAbilitiesH		= ""
+		fAbilities 		= ""
+		fEvolutions		= ""
+
+		titleTypes		= "Type(s)"
+		titleAbilities	= "Abilities"
+		titleEvolutions	= "Evolution Line"
+
+		for x in pTypes:
+			fTypes += x + ", "
+		fTypes = fTypes[:-2]
+		for x in pAbilities["normal"]:
+			pAbilitiesN += x + ", "
+		for x in pAbilities["hidden"]:
+			pAbilitiesH += x + ", "
+		fAbilities = "Normal: " + pAbilitiesN[:-2] + "\n" + "Hidden: " + pAbilitiesH[:-2]
+		for x in pEvolutions:
+			fEvolutions += x + ", "
+		fEvolutions = fEvolutions[:-2]
+
+		embed = discord.Embed	(	title	= pSpecies 			,
+							  		colour	= embedColor		)
+		embed.set_author		(	name	= pName 			,
+						 			url		= embedLink 		,
+						 			icon_url= embedIcon 		)
+		embed.set_thumbnail 	(	url		= pThumbnail 		)
+		embed.add_field 		(	name 	= titleTypes 		,
+									value 	= fTypes			)
+		embed.add_field			(	name 	= titleAbilities 	, 
+									value 	= fAbilities		)
+		embed.add_field			(	name 	= titleEvolutions 	, 
+									value 	= fEvolutions		)
+		embed.set_footer 		(	text 	= embedFooter 		, 
+									icon_url= embedFooterIcon 	)
+
+		await ctx.send(content	= msgContent, embed = embed)
+
+	@commands.command(hidden=True)
+	async def pokeList(self, ctx, *, content:str):
+		'''
+		Pokédex lookup tool, returns information about a pokémon in the form of a python list
+
+		Usage  : pokedex <number>
+		number : Pokémon's Ndex number
+		'''
+		params = content.split()
+		try:
+			int(params[0])
+		except ValueError:
+			await ctx.send("NaN")
+			return
+		currPoke = pokedex.get_pokemon_by_number(int(params[0]))
+		await ctx.send(currPoke)
+
+
+def setup(bot):
+	bot.add_cog(Games(bot))
